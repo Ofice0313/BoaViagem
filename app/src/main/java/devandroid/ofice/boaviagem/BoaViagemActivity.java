b64a8bbc3c64ab5dc6737f5092eec333825c4173
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2024-10-20 23:48:21 +0200

    Rewrite the methods of button and additing the methods of the on the method onCreate

--- app/src/main/java/devandroid/ofice/boaviagem/BoaViagemActivity.java
+++ app/src/main/java/devandroid/ofice/boaviagem/BoaViagemActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ public class BoaViagemActivity extends Activity {
 
     private EditText usuario;
     private EditText senha;
+    private Button btnEntrar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,19 +23,29 @@ public class BoaViagemActivity extends Activity {
 
         usuario = (EditText) findViewById(R.id.user);
         senha = (EditText) findViewById(R.id.senha);
+        btnEntrar = (Button) findViewById(R.id.btnLogin);
+
+        entrarOnClick();
     }
 
-    public void entrarOnClick(View v){
-        String usuarioInformado = usuario.getText().toString();
-        String senhaInformada = senha.getText().toString();
+    public void entrarOnClick(){
 
-        if("caleb".equals(usuarioInformado) && "1234".equals(senhaInformada)){
-            startActivity(new Intent(this, DashboardActivity.class));
-        }else{
-            String mensagemErro = getString(R.string.erro_autenticacao);
-            Toast toast = Toast.makeText(this, mensagemErro, Toast.LENGTH_SHORT);
-            toast.show();
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuarioInformado = usuario.getText().toString();
+                String senhaInformada = senha.getText().toString();
 
-        }
+                if("caleb".equals(usuarioInformado) && "1234".equals(senhaInformada)){
+                    startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                }else{
+                    String mensagemErro = getString(R.string.erro_autenticacao);
+                    Toast toast = Toast.makeText(getApplicationContext(), mensagemErro, Toast.LENGTH_SHORT);
+                    toast.show();
+
+                }
+            }
+        });
+
     }
 }
