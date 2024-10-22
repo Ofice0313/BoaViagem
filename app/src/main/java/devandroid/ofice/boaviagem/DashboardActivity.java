c2cd67968930f1a5c3b8da29cdf0be9dc6be4092
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2024-10-22 09:03:11 +0200

    Creating the layout of register new trip and the class ViagemActivity

--- app/src/main/java/devandroid/ofice/boaviagem/DashboardActivity.java
+++ app/src/main/java/devandroid/ofice/boaviagem/DashboardActivity.java
@@ -1,8 +1,12 @@
 package devandroid.ofice.boaviagem;
 
+import static devandroid.ofice.boaviagem.R.id.nova_viagem;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,16 +14,31 @@ import androidx.annotation.Nullable;
 
 public class DashboardActivity extends Activity {
 
+    private Button selectOption;
+    TextView novaViagem;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+        novaViagem = findViewById(nova_viagem);
+        selecionarOpcao();
     }
 
-    public void selecionarOpcao(View v){
 
-        TextView textView = (TextView) v;
-        String opcao = "Opção: " + textView.getText().toString();
-        Toast.makeText(this, opcao, Toast.LENGTH_LONG).show();
+    public void selecionarOpcao(){
+
+        novaViagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.nova_viagem:
+                        startActivity(new Intent(this,ViagemActivity.class));
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + v.getId());
+                }
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/devandroid/ofice/boaviagem/ViagemActivity.java
@@ -0,0 +1,21 @@
+package devandroid.ofice.boaviagem;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class ViagemActivity extends Activity {
+
+    private Button btnSelecionar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastrar_nova_viagem);
+    }
+
+}
